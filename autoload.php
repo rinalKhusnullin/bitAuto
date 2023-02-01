0f3099212eb3328893bae897eec7d373302e7578
Daniil-Tevs <daniil_tevs@mail.ru>
2023-02-01 21:20:19 +0200

    autoload.php: refactor - add array $directories, where now store directories '/srs/' and '/core/' for autoload classes from this folders

--- autoload.php
+++ autoload.php
@@ -3,18 +3,25 @@
 spl_autoload_register(function ($class) {
 	$prefix = 'ES\\';
 
-	$base_dir = __DIR__ . '/src/';
+	$directories = ['/src/', '/core/'];
 
 	$len = strlen($prefix);
-	if (strncmp($prefix, $class, $len) !== 0) {
+	if (strncmp($prefix, $class, $len) !== 0)
+	{
 		return;
 	}
 
 	$relative_class = substr($class, $len);
 
-	$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+	foreach ($directories as $directory)
+	{
+		$base_dir = __DIR__ . $directory;
+		$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
 
-	if (file_exists($file)) {
-		require  $file;
+		if (file_exists($file))
+		{
+			require $file;
+			break;
+		}
 	}
 });
