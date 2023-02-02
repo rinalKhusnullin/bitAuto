e5d6df4a3a52d96f5d4a74099072dea0a0047a0f
Daniil-Tevs <daniil_tevs@mail.ru>
2023-02-02 19:58:37 +0200

    TemplateEngine.php: refactor - change Exception to \RuntimeException

--- src/View/TemplateEngine.php
+++ src/View/TemplateEngine.php
@@ -2,20 +2,22 @@
 
 namespace ES\View;
 
+use http\Exception\RuntimeException;
+
 class TemplateEngine
 {
 	public static function view(string $path, array $variables = []): string
 	{
 		if (!preg_match('/^[0-9A-Za-z\/_-]+$/', $path))
 		{
-			throw new Exception("Invalid template path");
+			throw new \RuntimeException("Invalid template path");
 		}
 
 		$absolutePath = ROOT . "/views/$path.php";
 
 		if (!file_exists($absolutePath))
 		{
-			throw new Exception('Template not found');
+			throw new \RuntimeException('Template not found');
 		}
 		ob_start();
 
