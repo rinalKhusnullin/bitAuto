cdac535c9db330bb4ffdd7ce18cb36bec2d41ab8
Хуснуллин Риналь <fredygames66@gmail.com>
2023-02-05 01:02:27 +0200

    src/View/layout.php : replace pagination and debug pathes

--- src/View/layout.php
+++ src/View/layout.php
@@ -11,9 +11,9 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="styles/reset.css">
-	<link rel="stylesheet" href="styles/style.css">
-	<link rel="shortcut icon" href="images/logo.ico" type="image/x-icon">
+	<link rel="stylesheet" href="/styles/reset.css">
+	<link rel="stylesheet" href="/styles/style.css">
+	<link rel="shortcut icon" href="/images/logo.ico" type="image/x-icon">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@200;300;400;500&display=swap" rel="stylesheet">
@@ -26,21 +26,21 @@
 		<div class="container">
 			<div class="header__logo">
 				<a href="/" class="header__logo-link"><!--link to main page-->
-					<img src="images/logo.svg" alt="" class="header__logo-img">
+					<img src="/images/logo.svg" alt="" class="header__logo-img">
 				</a>
 			</div>
 			<div class="header__search">
 				<form class="header__search-form" action="/" method="get"><!--get request to search into database-->
 					<input class="header__search-input" type="text" name="search_query" placeholder="Введите название автомобиля">
 					<button class="header__search-btn">
-						<img src="images/search-icon.svg" alt="search" class="header__search-icon">
+						<img src="/images/search-icon.svg" alt="search" class="header__search-icon">
 					</button>
 				</form>
 			</div>
 			<div class="header__address">
 				<a href="https://yandex.ru/maps/org/bitriks24/1126159654/?ll=20.488308%2C54.719150&z=16.67" 
 					class="header__address-link" target="_blank"> <!--link to yandex maps with bitrix24 -->
-					<img src="images/mark-map.svg" alt="" class="header__address-icon icon">
+					<img src="/images/mark-map.svg" alt="" class="header__address-icon icon">
 				</a>
 				<p class="header__address-description">
 					Калининград <br> Гостиная, 3
@@ -48,7 +48,7 @@
 			</div>
 			<div class="header__contacts">
 				<a href="tel:+78002501860" class="header__contacts-link"> <!-- bitrix24 tel -->
-					<img src="images/contacts.svg" alt="" class="header__contacts-icon icon">
+					<img src="/images/contacts.svg" alt="" class="header__contacts-icon icon">
 				</a>
 				<p class="header__contacts-description">
 					8-800-250-18-60
@@ -82,17 +82,6 @@
 
 		<?= $content ?>
 
-		<div class="pagination">
-			<a href="##" class="pagination__item pagination-active">1</a>
-			<a href="##" class="pagination__item">2</a>
-			<a href="##" class="pagination__item">3</a>
-			<a href="##" class="pagination__item">4</a>
-			<span class="pagination__item-dots"> . . . </span>
-			<a href="##" class="pagination__item">13</a>
-			<a href="##" class="pagination__item">14</a>
-			<a href="##" class="pagination__item">15</a>
-		</div>
-
 	</div>
 </section>
 <footer class="footer">
@@ -100,7 +89,7 @@
 		<ul class="footer__info-list">
 			<li class="footer__info-item">
 				<div class="info-item__img">
-					<img src="images/team/matvey-img.png" alt="" class="info-item__icon">
+					<img src="/images/team/matvey-img.png" alt="" class="info-item__icon">
 				</div>
 				<div class="info-item__description">
 					<div class="info-item__socials">
@@ -115,7 +104,7 @@
 			</li>
 			<li class="footer__info-item">
 				<div class="info-item__img">
-					<img src="images/team/gleb-img.png" alt="" class="info-item__icon">
+					<img src="/images/team/gleb-img.png" alt="" class="info-item__icon">
 				</div>
 				<div class="info-item__description">
 					<div class="info-item__socials">
@@ -130,7 +119,7 @@
 			</li>
 			<li class="footer__info-item">
 				<div class="info-item__img">
-					<img src="images/team/tatyana-img.png" alt="" class="info-item__icon">
+					<img src="/images/team/tatyana-img.png" alt="" class="info-item__icon">
 				</div>
 				<div class="info-item__description">
 					<div class="info-item__socials">
@@ -145,7 +134,7 @@
 			</li>
 			<li class="footer__info-item">
 				<div class="info-item__img">
-					<img src="images/team/daniil-img.png" alt="" class="info-item__icon">
+					<img src="/images/team/daniil-img.png" alt="" class="info-item__icon">
 				</div>
 				<div class="info-item__description">
 					<div class="info-item__socials">
@@ -160,7 +149,7 @@
 			</li>
 			<li class="footer__info-item">
 				<div class="info-item__img">
-					<img src="images/team/rinal-img.png" alt="" class="info-item__icon">
+					<img src="/images/team/rinal-img.png" alt="" class="info-item__icon">
 				</div>
 				<div class="info-item__description">
 					<div class="info-item__socials">
@@ -180,7 +169,7 @@
 		</div>
 	</div>
 </footer>
-<script src="scripts/dropdown.js"></script>
+<script src="/scripts/dropdown.js"></script>
 </body>
 
 </html>
