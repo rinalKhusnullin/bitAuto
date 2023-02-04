d74cb6c60af322f2106d2e23dc253b4548adfb26
Хуснуллин Риналь <fredygames66@gmail.com>
2023-02-05 01:03:13 +0200

    src/View/components/pagination.php : add base pagination

---
+++ src/View/components/pagination.php
@@ -0,0 +1,10 @@
+<div class="pagination">
+    <a href="##" class="pagination__item pagination-active">1</a>
+    <a href="##" class="pagination__item">2</a>
+    <a href="##" class="pagination__item">3</a>
+    <a href="##" class="pagination__item">4</a>
+    <span class="pagination__item-dots"> . . . </span>
+    <a href="##" class="pagination__item">13</a>
+    <a href="##" class="pagination__item">14</a>
+    <a href="##" class="pagination__item">15</a>
+</div>
