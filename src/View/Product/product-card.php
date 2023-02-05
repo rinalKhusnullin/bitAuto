560ffef66ecfbcf92e95a325ac6786bfc6585c22
Хуснуллин Риналь <fredygames66@gmail.com>
2023-02-06 01:45:16 +0200

    View/components/product-card.php : refactor

--- src/View/Product/product-card.php
+++ src/View/Product/product-card.php
@@ -1,43 +1,43 @@
 <?php
 /**
- * @var int $Id ,
- * @var string $Brand ,
- * @var string $Name ,
- * @var string $Carcase ,
- * @var string $Transmission ,
- * @var int $Price
+ * @var int $id ,
+ * @var string $brand ,
+ * @var string $title ,
+ * @var string $carcaseType ,
+ * @var string $transmission ,
+ * @var int $price
  */
 ?>
 
 <div class="product-card">
 	<div class="product-card__img-block">
-		<img src="/tmp-autoimg/auto-id-<?= $Id ?>.png" alt="" class="product-card__img">
+		<img src="/tmp-autoimg/auto-id-<?= $id ?>.png" alt="" class="product-card__img">
 	</div>
 	<div class="product-card__description">
 		<h2 class="product-card__title">
-			<a href="/" class="product-card__hlink"><?= $Name ?></a> <!-- link to detailed page -->
+			<a href="/product/<?= $id ?>" class="product-card__hlink"><?= $title ?></a>
 		</h2> 
 		<!-- chr - its characteristics -->
 		<ul class="product-card__chr-list">
 			<li class="product-card__chr">
 				<div class="product-card__chr-title">Марка</div>
-				<div class="product-card__chr-value"><?= $Brand ?></div>
+				<div class="product-card__chr-value"><?= $brand ?></div>
 			</li>
 
 			<li class="product-card__chr">
 				<div class="product-card__chr-title">Тип кузова</div>
-				<div class="product-card__chr-value"><?= $Carcase ?></div>
+				<div class="product-card__chr-value"><?= $carcaseType ?></div>
 			</li>
 
 			<li class="product-card__chr">
 				<div class="product-card__chr-title">КПП</div>
-				<div class="product-card__chr-value"><?= $Transmission ?></div>
+				<div class="product-card__chr-value"><?= $transmission ?></div>
 			</li>
 			<li class="product-card__chr">
 				<div class="product-card__chr-title">Цена</div>
-				<div class="product-card__chr-value"><?= $Price ?></div>
+				<div class="product-card__chr-value"><?= $price ?></div>
 			</li>
 		</ul>
-		<a href="/product" class="product-card__more">Подробнее</a> <!-- generate new product detailed page with id -->
+		<a href="/product/<?= $id ?>" class="product-card__more">Подробнее</a>
 	</div>
 </div>
