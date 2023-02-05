d77a7d51b21da6a51518ffaceb03ec4f0852fcef
Daniil-Tevs <daniil_tevs@mail.ru>
2023-02-05 13:48:52 +0200

    ProductController.php: add control of detail page

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ES\Controller;
+
+use ES\Model\sqlDB;
+
+class ProductController extends BaseController
+{
+	public function getDetailAction(int $id): void
+	{
+		$db = new sqlDB();
+		$product = $db->getDataByID($id);
+
+		$this->render('layout', [
+			'title' => Option::getConfig('TITLE'),
+			'content' => TemplateEngine::view('Product/product-detailed', ['product' => $product,]),
+		]);
+	}
+}
