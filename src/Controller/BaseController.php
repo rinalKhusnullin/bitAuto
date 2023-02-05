bb4d62afbef6dcb73d623e1473382ac3abebaee7
Daniil-Tevs <daniil_tevs@mail.ru>
2023-02-05 13:46:20 +0200

    BaseController.php: add base controller with function render

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ES\Controller;
+
+class BaseController
+{
+	public function render(string $templateName, array $data)
+	{
+		if (!preg_match('/^[0-9A-Za-z\/_-]+$/', $templateName))
+		{
+			throw new \RuntimeException("Invalid template path");
+		}
+
+		$absolutePath = ROOT . "/src/View/" . $templateName . ".php";
+
+		if (!file_exists($absolutePath))
+		{
+			throw new \RuntimeException("Template {$templateName} not found");
+		}
+		ob_start();
+
+		extract($data, EXTR_OVERWRITE);
+
+		require $absolutePath;
+	}
+}
