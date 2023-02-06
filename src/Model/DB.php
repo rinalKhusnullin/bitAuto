0fc17883acfbb65cfde683a2cb7275b36f2c9742
Хуснуллин Риналь <fredygames66@gmail.com>
2023-02-06 21:41:26 +0200

    debug into db

--- src/Model/DB.php
+++ src/Model/DB.php
@@ -6,12 +6,12 @@ abstract class DB
 {
 	abstract function connect();
 	abstract function getData() : array;
-	abstract function getDataByID($id) : Products\Product;
+	abstract function getDataByID($id) : ?Products\Product;
 	abstract function getDataByTeg() : array;
 	abstract function updateData();
 	abstract function createData();
 	abstract function deleteData();
-	abstract function buildProduct($result,$connection) :array;
+	abstract function buildProduct($result,$connection) : ?array;
 	abstract function getPageCount();
 	abstract function createOrder();
 }
