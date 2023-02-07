cdbabef0cdb4755abd28a72a8961a619c166f142
Daniil-Tevs <daniil_tevs@mail.ru>
2023-02-07 18:58:26 +0200

    ProductController.php: add work with post data

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,31 +2,39 @@
 
 namespace ES\Controller;
 
+use ES\Model\Order;
 use ES\Model\sqlDB;
 
 class ProductController extends BaseController
 {
 	public function getDetailAction($id): void
 	{
-		if ((int)$id === 0) 
-		{
-			header('Location: /error');
-		}
+		(is_numeric($id)) ? $id = (int)$id : header('Location: /error');
 
 		$db = new sqlDB();
-		$product = $db->getDataByID($id);
+		$product = $db->getDataByID((int)$id);
 
-		if (!$product) 
+		if ($product === null)
 		{
 			header('Location: /error');
 		}
 
-		
-		// echo '<pre>';
-		// print_r($product);
-		// echo print_r($_POST);
-		// echo '</pre>';
-		
+		if (!empty($_POST))
+		{
+			$db->createOrder(new Order(
+				$_POST['userLastname'],
+				$_POST["userName"],
+				$_POST["userTel"],
+				$_POST["userEmail"],
+				$_POST["userAddress"],
+				$_POST["userComment"],
+				$product,
+				date('Y-m-d H:i:s')
+			));
+
+			header('Location: /byed');
+		}
+
 		$this->render('layout', [
 			'title' => ConfigurationController::getConfig('TITLE'),
 			'content' => TemplateEngine::view('Product/product-detailed', (array)$product),
