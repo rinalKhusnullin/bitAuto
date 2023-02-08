12b4fd08e8d723df50dabc304ee6106882c9388d
Хуснуллин Риналь <fredygames66@gmail.com>
2023-02-08 23:46:48 +0200

    View/pages/[failed.php | success.php] : add pages to display order status

---
+++ src/View/pages/failed.php
@@ -0,0 +1,9 @@
+<div class="order-status">
+    <div class="order-status__img">
+        <img src="/images/order-status/failed.jpg" alt="" srcset="">
+    </div>
+    <div class="order-status__desc-block">
+        <div class="order-status__title">Не удалось оформить заказ :(</div>
+        <div class="order-status__description">Извините, оформить заказ не удалось.<br> Попробуйте позже или свяжитесь с Комнадой2</div>
+    </div>
+</div>
