a4e9aa904606a5d04691433ea21d9d74deceade5
Хуснуллин Риналь <fredygames66@gmail.com>
2023-02-09 02:30:34 +0200

    Controller/IndexController.php : add sort by tags and seach query

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,10 +8,35 @@ class IndexController extends BaseController
 {
 	public function indexAction(): void
 	{
-		$indexPage = (isset($_GET['page']))? (int)$_GET['page']: 0;
-
+		$indexPage = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 		$db = new sqlDB();
-		$products = $db->getData(true, $indexPage);
+
+		if (isset($_GET['brand']) || isset($_GET['transmission']) || isset($_GET['carcase'])) //Если пользователь выбрал категории
+		{
+			$brand = isset($_GET['brand']) ? $_GET['brand'] : null; 
+			$carcase = isset($_GET['carcase']) ? $_GET['carcase'] : null;
+			$transmission =  isset($_GET['transmission']) ? $_GET['transmission'] : null;
+
+			// Возвращает массив из товаров и количества позиций
+			[$products, $pageCount] = $db->getDataByTeg($brand, $carcase, $transmission, $indexPage);
+		}
+		else if (isset($_GET['search_query']))
+		{
+			$searchQuery = $_GET['search_query'];
+
+			//Ищем по поисковой строке
+			[$products, $pageCount] = $db->getDataBySQuery($searchQuery, $indexPage); 
+		}
+		else
+		{
+			// Иначе получаем все товары
+			[$products, $pageCount] = [$db->getData(true, $indexPage), $db->getPageCount()];
+		}
+
+		if (empty($products))
+		{
+			// По хорошему тут нужно вывести что товары не найдены
+		}
 
 		$this->render('layout', [
 			'title' => ConfigurationController::getConfig('TITLE'),
@@ -19,7 +44,7 @@ class IndexController extends BaseController
 				'products' => $products,
 				'pagination' => TemplateEngine::view('components/pagination', [
 					'currentPage' => $indexPage,
-					'countPage' => $db->getPageCount(),
+					'countPage' => $pageCount,
 				]),
 			]),
 		]);
