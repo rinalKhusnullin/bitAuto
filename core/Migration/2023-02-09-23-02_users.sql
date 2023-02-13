94dde5ea9e0cb535895dbc2d754227982e26b098
Gleb <catinhat001@gmail.com>
2023-02-13 20:41:54 +0200

    change migration for users

--- core/Migration/2023-02-09-23-02_users.sql
+++ core/Migration/2023-02-09-23-02_users.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS users
 (
 	ID int not null auto_increment,
-	PASS char(40) not null,
+	PASS nvarchar(200) not null,
 	LOGIN nvarchar(50) not null,
+    FIRST_NAME nvarchar(50) not null,
+    LAST_NAME nvarchar(50) not null,
 	MAIL nvarchar(50) not null,
 	ROLE nvarchar (50) default 'user',
 	PRIMARY KEY (ID)
---
+++ core/Migration/2023-02-13-20-30_insertIntoUsers.sql
@@ -0,0 +1,2 @@
+insert into users (id, pass, login, mail, role, first_name, last_name)
+values (1, '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','admin','dev@dev.bx','Admin','Komand','2');
