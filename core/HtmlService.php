a3b8d0335f7c08bfd0ff11921ec878ab8cf778af
VolkovMatvey <matavech@icloud.com>
2023-02-15 00:06:52 +0200

    added one more case to better understand maybe its bad idk

--- core/HtmlService.php
+++ core/HtmlService.php
@@ -5,6 +5,13 @@ namespace ES;
 class HtmlService
 {
  public static function getHtmlTag($item, $key) {
+	 $db= new Model\sqlDAO\sqlDB();
+	 $categories = $db->getTegs();
+	 foreach ($categories[0] as $category)
+	 {
+		 $brand[] = $category['BRAND'];
+	}
+	// ADD OTHER CATEGORIES PLEASE
 	 switch ($key) {
 		 case 'id' : return $item;
 
@@ -12,6 +19,15 @@ class HtmlService
 		 case 'title' : return '<input class="admin-input" type="text" value=" '. $item . ' ">';
 
 		 case 'isActive' : return '<select> <option selected="selected">Да</option><option>Нет</option></select>';
+
+		 case 'brand' : {
+			 $result = '<select>';
+			 foreach ($brand as $item) {
+				 $result .= '<option> ' . $item . '</option>';
+			 }
+			 $result .= '</select>';
+			 return $result;
+		 }
 
 		 case 'fullDesc' : return '<textarea rows = "10" cols="45"> '. $item. ' </textarea>';
 	 }
