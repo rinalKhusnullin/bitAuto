ee9971820194438cc3cd04800534ad493bd33ef1
Tyutereva Tanya <rnnchik@gmail.com>
2023-02-18 20:36:29 +0200

    changed error message

--- src/View/pages/login.php
+++ src/View/pages/login.php
@@ -8,9 +8,9 @@
 
 <form action="/login/" method="post">
 	<?php if($errors):?>
-		<div>
-			<?=$errors[0]?>
-		</div>
+		<?php foreach ($errors as $error) :?>
+			<div><?= $errors?></div>
+		<?php endforeach; ?>
 	<?php endif; ?>
 	<input required type="text" name="login" placeholder="login">
 	<input required type="password" name="password" placeholder="password">
