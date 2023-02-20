cf34e865b5ea6149c0bc791a5d3b09e111146271
Daniil-Tevs <daniil_tevs@mail.ru>
2023-02-20 22:29:56 +0200

    slider.php: make slider dynamic

--- src/View/components/slider.php
+++ src/View/components/slider.php
@@ -1,18 +1,19 @@
 <?php
-/** 
+/**
  * @var int $id
+ * @var array $images
  */
 ?>
 <div class="swiper">
-    <div class="swiper-wrapper">
-        <?php for ($i = 1; $i < 4; $i++): ?>
-            <div class="swiper-slide">
-                <img src="/tmp-autoimg/<?="$id/$i"?>.png" alt="">
-            </div>
-        <?php endfor ?>
-    </div>
-    <div class="swiper-pagination"></div>
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
+	<div class="swiper-wrapper">
+		<?php foreach ($images as $image): ?>
+			<div class="swiper-slide">
+				<img src="/tmp-autoimg/<?="$id/$image" ?>" alt="">
+			</div>
+		<?php endforeach; ?>
+	</div>
+	<div class="swiper-pagination"></div>
+	<div class="swiper-button-next"></div>
+	<div class="swiper-button-prev"></div>
 </div>
 <script src="/scripts/slider.js"></script>
