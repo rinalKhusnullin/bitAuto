1df10f4e3d3e4f6c19f374c52b6324be08cb9716
Daniil-Tevs <daniil_tevs@mail.ru>
2023-02-23 18:13:57 +0200

    admin-edit.php: remake from admin-edit-rows.php

--- src/View/pages/admin-edit.php
+++ src/View/pages/admin-edit.php
@@ -3,18 +3,32 @@
  * @var $tableName
  * @var array $columns
  * @var $content
+ * @var int $len
+ * @var $className
  */
-if(is_array($columns))
+if (is_array($columns))
 {
 ?>
 <table>
 	<caption> <?= $tableName ?> </caption>
-	<tr>
-		<?php foreach ($columns as $column) {?>
-			<th><?= $column?></th>
-		<?php } ?>
-		<th class="td__change"></th>
-
-	</tr>
-	<?php } echo $content; ?>
+	<tbody class="tbody_edit">
+		<form action = "" method="POST">
+		<?php for($i=0;$i<$len;$i++):?>
+			<tr class="tr_edit">
+				<th class="th_edit"><?= $columns[$i] ?></th>
+				<input type="hidden" name="item" value="<?= $className ?>">
+				<td class="td_edit"> <?= ES\HtmlService::getHtmlTag($columns[$i], $content[$columns[$i]]) ?> </td>
+			</tr>
+				<?php endfor;?>
+			<tr class="tr_edit">
+			<th class="th_edit"></th>
+			<td class="td_edit"><button type="submit">Сохранить</button></td>
+			</tr>
+			<?php if (is_object($content)): ?>
+				<td>
+					<a href="/admin/edit/delete/?<?= array_key_first($_GET)?>=<?= $content->id?>">удалить</a>
+				</td>
+			<?php endif; }?>
+		</form>
+	</tbody>
 </table>
