82c0c98c7f2177294709dc21cd7ee57873ab1ad6
Хуснуллин Риналь <fredygames66@gmail.com>
2023-02-24 13:55:11 +0200

    View/pages/login.php : add styles to login

--- src/View/pages/login.php
+++ src/View/pages/login.php
@@ -5,13 +5,14 @@
 ?>
 
 
-<form action="/login/" method="post">
+<form class="auth" action="/login/" method="post">
+	<h1 class="auth_title"> SIGN IN </h1>
 	<?php if($errors):?>
 		<?php foreach ($errors as $error) :?>
-			<div><?= $error?></div>
+			<div class="error"><?= $error ?></div>
 		<?php endforeach; ?>
 	<?php endif; ?>
-	<input required type="text" name="login" placeholder="login">
-	<input required type="password" name="password" placeholder="password">
-	<button>Sign in</button>
+	<input required type="text" name="login" placeholder="Логин" class="login">
+	<input required type="password" name="password" placeholder="Пароль" class="password">
+	<button class="sign">Войти</button>
 </form>
