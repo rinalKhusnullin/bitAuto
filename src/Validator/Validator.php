4ad2a9adeaaf1203fb7dda899b714969d3d29e8a
Хуснуллин Риналь <fredygames66@gmail.com>
2023-02-26 01:22:13 +0200

    Validator.php : delete func isValidName()

--- src/Validator/Validator.php
+++ src/Validator/Validator.php
@@ -4,14 +4,9 @@ namespace ES\Validator;
 
 class Validator
 {
-    static function isValidName(string $fullName) : bool
-    {
-        return true; // нужно по хорошему регулярку сюда запилить для проверки валидного имени (без цифр и прочей чепухи)
-    }
-
     static function isValidNumberPhone(string $numberPhone) : bool
     {
-        return preg_match("#^(\+7|8)[0-9]{10}$#", $numberPhone);
+        return preg_match("#^7[0-9]{10}$#", $numberPhone);
     }
 
     static function isValidEmail(string $email) : bool
