9dd96d49a6663e4aa43f965649860027e2d9c53b
Хуснуллин Риналь <fredygames66@gmail.com>
2023-02-28 23:01:52 +0200

    add validate for string length into js

--- public/scripts/editValidate.js
+++ public/scripts/editValidate.js
@@ -1,12 +1,29 @@
+const TAGSTOVALIDATE = ['INPUT', 'TEXTAREA'];
+const TOVALIDATE = ['title', 'fullDesc', 'price', 'value', 'login', 
+    'firstName', 'lastName', 'mail', 'fullName', 'phone', 'address', 'productId', 'productPrice'];
+
+const TITLE_RESTRICK = 255 - 1;
+const FULLDESC_RESTRICK = 2000 - 1;
+const VALUE_RESTRICK = 50 - 1;
+const FULLNAME_RESTRICK = 100 - 1;
+const PHONE_RESTRICK = 15 - 1;
+const MAIL_RESTRICK = 50 - 1;
+const COMMENT_RESTRICK = 500 - 1;
+const ADDRESS_RESTRICK = 80 - 1;
+
+const INT_RESTRICK = 11 - 1;  
+
+const NOTCHARS = ['price', 'productId', 'productPrice', 'phone']
+
 function editValidate()
 {
     wasNoError = true;
     form = document.forms['admin_edit'];
     for (let i = 0; i < form.length; i++)
     {
-        if (form[i].nodeName === 'INPUT' || form[i].nodeName === 'TEXTAREA')
+        if (TAGSTOVALIDATE.includes(form[i].nodeName))
         {
-            if (form[i].type === 'text')
+            if (TOVALIDATE.includes(form[i].name))
             {
                 if (form[i].value === '')
                 {
@@ -27,26 +44,47 @@ function editValidate()
     return wasNoError;
 }
 
-input = document.querySelectorAll('input, textarea');
+input = document.forms['admin_edit'];
 
-input.forEach(element => {
+[...input].forEach(element => {
     element.addEventListener('input', function() {
-        if (element.name === 'price')
+        if (TAGSTOVALIDATE.includes(element.nodeName) && TOVALIDATE.includes(element.name))
         {
-            element.value = element.value.replace(/[^\d]/g,'');
-        }
-        
-        if (element.value === '')
-        {
-            elementName = element.name.toUpperCase();
-            element.parentElement.classList.add('empty');
-            element.classList.add('red-placeholder');
-            element.placeholder = 'Please, enter ' + elementName;
-        } 
-        else
-        {
-            element.parentElement.classList.remove('empty');
-            element.placeholder = '';
+            if (NOTCHARS.includes(element.name))
+            {
+                element.value = element.value.replace(/[^\d]/g,'');
+            }
+            
+            if (element.value === '')
+            {
+                elementName = element.name.toUpperCase();
+                element.parentElement.classList.add('empty');
+                element.classList.add('red-placeholder');
+                element.placeholder = 'Please, enter ' + elementName;
+            } 
+            else
+            {
+                element.parentElement.classList.remove('empty');
+                element.placeholder = '';
+            }
+
+            if (element.name === 'price') element.value = element.value.substring(0, INT_RESTRICK);
+
+            if (element.name === 'title') element.value = element.value.substring(0, TITLE_RESTRICK);
+
+            if (element.name === 'fullName') element.value = element.value.substring(0, FULLNAME_RESTRICK);
+
+            if (element.name === 'phone') element.value = element.value.substring(0, PHONE_RESTRICK);
+
+            if (element.name === 'mail') element.value = element.value.substring(0, MAIL_RESTRICK);
+
+            if (element.name === 'address') element.value = element.value.substring(0, ADDRESS_RESTRICK);
+
+            if (element.name === 'comment') element.value = element.value.substring(0, COMMENT_RESTRICK);
+
+            if (element.name === 'productId') element.value = element.value.substring(0, INT_RESTRICK);
+
+            if (element.name === 'productPrice') element.value = element.value.substring(0, INT_RESTRICK);
         }
     })
 });
